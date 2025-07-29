432b5e53bc29212b72912aad3812492aefca3c64
skbasha-devops <khaju452@gmail.com>
2025-07-29 23:32:12 +0530

    Update file.tf

--- file.tf
+++ file.tf
@@ -1,6 +1,6 @@
-resource "local_file" res1 {
-filename = var.f1
-content = var.f1
+resource "local_file" "res1" {
+  filename = var.f1
+  content  = var.f1  # CORRECT: using equals sign
 }
 variable f1 {
 type = string
